740f49fa2b10262756b6976fc88a47007fe15cf3
sergbeskr <sergbeskr04081993@gmail.com>
2015-12-15 19:51:37 +0200

    Update copy_many_db_files.sh

--- copy_many_db_files.sh
+++ copy_many_db_files.sh
@@ -1,7 +1,7 @@
 folder=`date +%d%m20%y_%H%M`
 mkdir -p $folder/2015
 
-for file in `ls -1 --sort=time`
+for file in `ls /var/data/2015 -1 --sort=time`
 do
 cp -r /var/data/2015/$file /var/backups/$folder/2015/$file
 done
