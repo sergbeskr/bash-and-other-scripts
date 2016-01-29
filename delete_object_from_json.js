cfd7d2a04835be425e6653db6681f5f89c616104
sergbeskr <sergbeskr04081993@gmail.com>
2016-01-29 19:08:18 +0200

    Update delete_object_from_json.js

--- delete_object_from_json.js
+++ delete_object_from_json.js
@@ -1,10 +1,10 @@
-var npmJson = require('/var/www/nodebbv0.9.3_2/npm-shrinkwrap.json')
+var npmJson = require('/opt/jenkins/jobs/nodebb-rpm/workspace/NodeBB-0.9.3/npm-shrinkwrap.json')
     fs  = require('fs').
     ;
 
 delete npmJson["dependencies"]["utf-8-validate"];
 delete npmJson["dependencies"]["bufferutil"];
 
-fs.writeFileSync("/var/www/nodebbv0.9.3_2/npm-shrinkwrap.json", JSON.stringify(npmJson, null, 2));
+fs.writeFileSync("/opt/jenkins/jobs/nodebb-rpm/workspace/NodeBB-0.9.3/npm-shrinkwrap.json", JSON.stringify(npmJson, null, 2));
 
 console.log("detete utf-8-validate and bufferutil from npm-shrinkwrap.json")
