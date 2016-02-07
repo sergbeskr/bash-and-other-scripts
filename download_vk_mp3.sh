9075daaffbd92ea6246ef1c4d77a0c29a0612ae8
sergbeskr <sergbeskr04081993@gmail.com>
2016-02-07 19:45:19 +0200

    Update download_vk_mp3.sh

--- download_vk_mp3.sh
+++ download_vk_mp3.sh
@@ -1,4 +1,4 @@
-cat 001 | sed 's/mp3/mp3\n/g' > 002
-
+#cat 001 | sed 's/mp3/mp3\n/g' > 002
 #cat 002 | grep -wo 'http.*\{0,70\}mp3' > 003
-cat 002 | grep -o  'http.\{0,70\}mp3' | sed 's/\\\\\\//g' > 003
+
+cat 001 | grep -o  'http.\{0,70\}mp3' | sed 's/\\\\\\//g' > 003
