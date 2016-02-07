5aebd16afbf13619a2d0f22e6e1ef2db2fa1cf00
sergbeskr <sergbeskr04081993@gmail.com>
2016-02-07 21:20:58 +0200

    Update download_vk_mp3.js

--- download_vk_mp3.js
+++ download_vk_mp3.js
@@ -1,12 +1,18 @@
 var fs = require('fs');
 var file = fs.readFileSync("001", {encoding:'utf-8'});
 var temp = require('stream');
+var mp3url = "";
 
 var adr =  /http.{0,70}mp3/g;
+//var adr =  /http.\{0,70\}mp3/g;
 
 var i=0;
 while(temp){
   temp=file.match(adr)[i];
-  console.log(temp);
-  i++;
+  if(temp){
+    mp3url=String(temp).replace(/\\\\\\/g,"");
+    mp3url=String(mp3url).replace(/undefined/g,"");
+    console.log(mp3url);
+    i++;
+  }
 }
