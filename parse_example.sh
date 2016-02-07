01514a6afc694ee6f7c1b4e0e78fd8ab87d8dded
sergbeskr <sergbeskr04081993@gmail.com>
2016-02-07 19:44:51 +0200

    Update parse_example.sh

--- parse_example.sh
+++ parse_example.sh
@@ -4,9 +4,8 @@ sed 's/yellow/green/g'
 # find yellowfin first pid
 ps ax | sed "s/grep.*//g" | grep 'yellowfin' | sed 's/ .*//g' | head -n 1
 
-# find all mp3 urls from something file
-cat 001 | sed 's/mp3/mp3\n/g' > 002
-cat 002 | grep -wo 'http.\{0,70\}mp3' | sed 's/\\\\\\//g' > 003
+# add newrow symbol
+sed 's/mp3/mp3\n/g'
 
 # always work...hmmm
-cat 002 | grep -o 'http.\{0,70\}mp3' | sed 's/\\\\\\//g' > 003 
+cat 001 | grep -o 'http.\{0,70\}mp3' | sed 's/\\\\\\//g' > 003 
