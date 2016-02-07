3de7ba66cfbf81fd4fa02c58d91e2317d3c1f469
sergbeskr <sergbeskr04081993@gmail.com>
2016-02-07 16:58:41 +0200

    Update parse_example.sh

--- parse_example.sh
+++ parse_example.sh
@@ -7,3 +7,6 @@ ps ax | sed "s/grep.*//g" | grep 'yellowfin' | sed 's/ .*//g' | head -n 1
 # find all mp3 urls from something file
 cat 001 | sed 's/mp3/mp3\n/g' > 002
 cat 002 | grep -wo 'http.\{0,70\}mp3' | sed 's/\\\\\\//g' > 003
+
+# always work...hmmm
+cat 002 | grep -o 'http.\{0,70\}mp3' | sed 's/\\\\\\//g' > 003 
