f5fc26af8806cfdc85f5861554a9ba6df69ae277
sergbeskr <sergbeskr04081993@gmail.com>
2016-03-13 21:28:00 +0200

    Update parse_example.sh

--- parse_example.sh
+++ parse_example.sh
@@ -1,6 +1,5 @@
 # replace yellow to green
 sed 's/yellow/green/g'
-sed 's/,/%2c/g'
 
 # find yellowfin first pid
 ps ax | sed "s/grep.*//g" | grep 'yellowfin' | sed 's/ .*//g' | head -n 1
@@ -10,3 +9,6 @@ sed 's/mp3/mp3\n/g'
 
 # always work...hmmm
 cat 001 | grep -o 'http.\{0,70\}mp3' | sed 's/\\\\\\//g' > 003 
+
+# replace some symbols in url
+sed 's/,/%2c/g'
