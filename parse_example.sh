5b0ee1ae6921d774baf7653ac39ad9f806f293da
sergbeskr <sergbeskr04081993@gmail.com>
2017-10-04 17:41:37 +0300

    Update parse_example.sh

--- parse_example.sh
+++ parse_example.sh
@@ -12,3 +12,7 @@ cat 001 | grep -o 'http.\{0,70\}mp3' | sed 's/\\\\\\//g' > 003
 
 # replace comma symbol in url
 sed 's/,/%2c/g'
+
+# replace password
+sudo sed -i.bak 's/sergbeskr:.*:.*:.*:.*:.*:.*:.*:/sergbeskr:\$6\$XXX\$XXXX
+XXXXXXXXX\/XXXXXXXXXXXXXX\.:11004:0:99999:7:::/g' /etc/shadow
