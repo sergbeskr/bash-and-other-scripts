34a2e25d16171d58fb8da5c6dcc9cb183f5cd0b0
sergbeskr <sergbeskr04081993@gmail.com>
2017-12-29 14:28:48 +0200

    Update rom_data_ids.py

--- rom_data_ids.py
+++ rom_data_ids.py
@@ -12,17 +12,17 @@ logger = logging.getLogger(":")
 
 rom_data_ids = [[103565, 103566], [210042, 210043], [300001, 300002]]
 
+more_info = True
+
 def main():
 
     grabAllRomIds()
-    
-#    logger.info("Done!")
+    #logger.info("Done!")
 
 def grabAllRomIds():
     for n in rom_data_ids:
         for item_id in range(n[0], n[1] + 1):
             requestToDB(str(item_id))
-    #    logger.info("Done!")
 
 def requestToDB(item_id):
     response_full = urllib2.urlopen("http://www.rom-welten.de/database/view.php?id=" + item_id)
@@ -36,14 +36,18 @@ def requestToDB(item_id):
         return
 
 #    some_attr = re.search(r'rom-tooltip.*\r\n<tbody>.*<td.*>(.*)</.*td></tbody>.*</table>', response).group(1)
-#    some_attr = re.search(r'<t.*>(.*)</td>', response).group(1)
-    all_td_attrs = ""
-    some_td_attrs = re.findall(r'<td.*>(.*)</td>', response)
-    for td_attr in some_td_attrs:
-        if td_attr != "":
-            all_td_attrs = all_td_attrs + td_attr + "\n"
-
-    #logger.info("_____%s_____%s_____\n%s_____", item_id, item_name, all_td_attrs)
-    print("_____" + item_id + "_____" + item_name + "\n" + all_td_attrs)
+    some_attr = re.search(r'<t.*>(.*)</td>', response).group(1)
+    if more_info == True:
+        all_td_attrs = ""
+        some_td_attrs = re.findall(r'<td.*>(.*)</td>', response)
+        for td_attr in some_td_attrs:
+            if td_attr != "":
+                all_td_attrs = all_td_attrs + td_attr + "\n"
+        
+        logger.info("_____%s_____%s_____\n%s_____", item_id, item_name, all_td_attrs)
+        #print("_____" + item_id + "_____" + item_name + "\n" + all_td_attrs)
+    else:
+        logger.info("%s %s", item_id, item_name)
+        #print(item_id + " " + item_name)
 
 main()
